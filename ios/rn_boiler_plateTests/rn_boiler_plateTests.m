ffe4797cec1df357c8f13875172ef17043c40deb
Daryu-Kim <dev_daryu@kakao.com>
2023-07-18 18:41:23 +0900

    Create

--- ios/rn_boiler_plateTests/rn_boiler_plateTests.m
+++ ios/rn_boiler_plateTests/rn_boiler_plateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rn_boiler_plateTests : XCTestCase
+@interface plancationTests : XCTestCase
 
 @end
 
-@implementation rn_boiler_plateTests
+@implementation plancationTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
